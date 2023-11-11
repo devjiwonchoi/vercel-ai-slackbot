087c7b4b4d4c9a00e1dd2fb66b403c704d9e423f
Jiwon Choi <devjiwonchoi@gmail.com>
2023-11-11 20:49:26 +0900

    slack gpt bot pm2 integration (#1)

--- src/askit.ts
+++ src/askit.ts
@@ -1,13 +1,12 @@
 import OpenAI from 'openai'
-import Slack from '@slack/bolt'
+import { App } from '@slack/bolt'
 
-const slack = new Slack.App({
+const slack = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
 })
 
-slack.start()
 slack.event('app_mention', async ({ event, say }) => {
   const gptResponse = await getGPTResponse(event.text)
   await say(`${gptResponse.choices[0].message.content}`)
@@ -20,3 +19,6 @@ async function getGPTResponse(text: string) {
     messages: [{ role: 'user', content: text }],
   })
 }
+
+slack.start()
+console.log('⚡️ Bolt app is running!')
