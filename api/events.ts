6d61f38237af0c09a241b7599b0d2cf985f679b6
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-14 15:26:50 +0900

    refactor: revert slack bolt

--- api/events.ts
+++ api/events.ts
@@ -1,4 +1,12 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { App as Slack } from '@slack/bolt'
+import { getGPTResponse, generatePromptFromThread } from '../src/openai'
+
+const { client } = new Slack({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+})
 
 export default async function events(
   request: VercelRequest,
@@ -11,26 +19,26 @@ export default async function events(
       return response.status(200).json({ challenge: request.body.challenge })
     }
 
-    // if (requestType === 'event_callback') {
-    //   const eventType = request.body.event.type
-    //   if (eventType === 'app_mention') {
-    //     const { channel, ts, thread_ts } = request.body.event
-    //     const thread = await client.conversations.replies({
-    //       channel,
-    //       ts: thread_ts ?? ts,
-    //       inclusive: true,
-    //     })
+    if (requestType === 'event_callback') {
+      const eventType = request.body.event.type
+      if (eventType === 'app_mention') {
+        const { channel, ts, thread_ts } = request.body.event
+        const thread = await client.conversations.replies({
+          channel,
+          ts: thread_ts ?? ts,
+          inclusive: true,
+        })
 
-    //     const prompt = await generatePromptFromThread(thread)
-    //     const gptResponse = await getGPTResponse(prompt)
+        const prompt = await generatePromptFromThread(thread)
+        const gptResponse = await getGPTResponse(prompt)
 
-    //     await client.chat.postMessage({
-    //       channel,
-    //       thread_ts: ts,
-    //       text: `${gptResponse.choices[0].message.content}`,
-    //     })
-    //   }
-    // }
+        await client.chat.postMessage({
+          channel,
+          thread_ts: ts,
+          text: `${gptResponse.choices[0].message.content}`,
+        })
+      }
+    }
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message)
