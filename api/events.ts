5c04e4f97f51708422cf7cd5dd5d623834250db8
devjiwonchoi <devjiwonchoi@gmail.com>
2024-01-14 21:59:54 +0900

    refactor: modify chat function name

--- api/events.ts
+++ api/events.ts
@@ -1,22 +1,25 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import chat from './_chat'
+import { sendGPTResponse } from './_chat'
 import { isValidSlackRequest } from './_validate-request'
 
 export default async function events(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const requestType = request.body.type
+  const {
+    challenge,
+    type: requestType,
+    event: { type: eventType },
+  } = request.body
 
   if (requestType === 'url_verification') {
-    return response.status(200).json({ challenge: request.body.challenge })
+    return response.status(200).json({ challenge })
   }
 
   if (isValidSlackRequest(request)) {
     if (requestType === 'event_callback') {
-      const eventType = request.body.event.type
       if (eventType === 'app_mention') {
-        await chat(request)
+        await sendGPTResponse(request)
       }
     }
   }
