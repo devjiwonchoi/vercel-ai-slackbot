bd62572ef20a209fd75a242483b8e88480f66dae
Jiwon Choi <devjiwonchoi@gmail.com>
2024-01-14 20:33:59 +0900

    feat: add validating request from slack (#8)

---
+++ api/_validate-request.ts
@@ -0,0 +1,22 @@
+// This code snippet is based on https://github.com/devjiwonchoi/vercel-slackbot/blob/main/api/_validate.js
+// To know more about validating requests from Slack, check out https://api.slack.com/authentication/verifying-requests-from-slack
+import crypto from 'crypto'
+const signingSecret = process.env.SLACK_SIGNING_SECRET!
+
+export function isValidSlackRequest(event: any) {
+  const { body, headers } = event
+  const requestBody = JSON.stringify(body)
+
+  const timestamp = headers['x-slack-request-timestamp']
+  const slackSignature = headers['x-slack-signature']
+  const baseString = 'v0:' + timestamp + ':' + requestBody
+
+  const hmac = crypto
+    .createHmac('sha256', signingSecret)
+    .update(baseString)
+    .digest('hex')
+  const computedSlackSignature = 'v0=' + hmac
+  const isValidRequest = computedSlackSignature === slackSignature
+
+  return isValidRequest
+}
--- api/events.ts
+++ api/events.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import chat from './_chat'
+import { isValidSlackRequest } from './_validate-request'
 
 export default async function events(
   request: VercelRequest,
@@ -11,10 +12,12 @@ export default async function events(
     return response.status(200).json({ challenge: request.body.challenge })
   }
 
-  if (requestType === 'event_callback') {
-    const eventType = request.body.event.type
-    if (eventType === 'app_mention') {
-      chat(request)
+  if (isValidSlackRequest(request)) {
+    if (requestType === 'event_callback') {
+      const eventType = request.body.event.type
+      if (eventType === 'app_mention') {
+        chat(request)
+      }
     }
   }
 }
