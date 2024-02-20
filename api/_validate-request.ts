3ab621ccc21d3b3c5afd7696e548bf6736d629ac
devjiwonchoi <devjiwonchoi@gmail.com>
2024-02-20 09:55:57 +0900

    refactor: ensure attacks or delayed prompts

--- api/_validate-request.ts
+++ api/_validate-request.ts
@@ -13,6 +13,15 @@ export async function isValidSlackRequest({
   const timestamp = request.headers.get('X-Slack-Request-Timestamp')
   const slackSignature = request.headers.get('X-Slack-Signature')
 
+  if (!timestamp || !slackSignature) {
+    return false
+  }
+
+  // Prevent replay attacks on the order of 5 minutes
+  if (Math.abs(Date.now() / 1000 - parseInt(timestamp)) > 60 * 5) {
+    return false
+  }
+
   const base = `v0:${timestamp}:${JSON.stringify(payload)}`
   const hmac = crypto
     .createHmac('sha256', signingSecret)
@@ -20,5 +29,9 @@ export async function isValidSlackRequest({
     .digest('hex')
   const computedSignature = `v0=${hmac}`
 
-  return computedSignature === slackSignature
+  // Prevent timing attacks
+  return crypto.timingSafeEqual(
+    Buffer.from(computedSignature),
+    Buffer.from(slackSignature)
+  )
 }
